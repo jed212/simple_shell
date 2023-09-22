797cd7644bc8ce72a2b882f876b6f6534343fa07
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-09-22 09:57:46 +0000

    added project files

--- shell.h
+++ shell.h
@@ -22,32 +22,30 @@ struct bltn_cmmnd
 	char *name;
 	int (*function)(char **array_of_tokens);
 };
-
-
-void prompt(void);
+void display_prompt(void);
 void _sig_int_handler_(int signal);
 ssize_t _getline(char **input,
 size_t *number_of_malloc_bytes_allocated, int status);
 char **_array_maker_(char *input, char *delimiter);
-int _fork(char *command, char **array_of_tokens);
-char *_which(char *filename);
-int _print_env(void);
-char *_getenv(const char *name);
-int _setenv(const char *name, const char *value, int overwrite);
-int _unsetenv(const char *name);
-char *malloc_char(char **string, size_t size_of_malloc, char *error_message);
-char **malloc_array(char **array, size_t size_of_malloc, char *error_message);
-int copy_array(char **destination, char **source);
-int _env_name_exists(const char *name);
+int _fork(char *cmd, char **token_arr);
+char *_which(char *file_name);
+int print_env(void);
+char *_getenv_(const char *n);
+int _setenv_(const char *n, const char *v,  int overwrite);
+int _unsetenv_(const char *n);
+char *malloc_char_(char **string, size_t size_of_malloc, char *error_msg);
+char **malloc_array_(char **array, size_t size_of_malloc, char *error_msg);
+int cpy_arr(char **destination, char **source);
+int name_exists(const char *n);
 int _env_length(void);
-void free_which(char **path_var, char **array_of_tokens);
-void free_main(char **array_of_tokens, char *input);
+void _free_which(char **path_var, char **token_arr);
+void _free_main(char **token_arr, char *input);
 void _rev_str_(char *str);
 char *__itoa__(size_t cmmnd_nmb);
 int _dgt_cntr_(size_t cmmnd_nmb);
-int error_not_found(char **arvs, char **array_of_tokens, size_t command_num);
-int _is_builtin_(char **array_of_tokens);
-int _bltn_handler_(char **array_of_tokens);
-int _exit_builtin(char **array_of_tokens);
+int error_not_found(char **arvs, char **token_arr, size_t cmd_num);
+int _exit_builtin(char **token_arr);
 int _env_builtin(char **array_of_tokens);
+int _bltn_handler_(char **array_of_tokens)
+int _is_builtin_(char **array_of_tokens);
 #endif
